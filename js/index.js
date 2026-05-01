4e2cc765fdf8dc8bcf4a4114dcb3c68287e63e4a
foyez khan <mdfoyezkn@gmail.com>
2026-05-01 12:27:50 +0600

    this is new funtional practce

--- js/index.js
+++ js/index.js
@@ -1,2 +1,15 @@
-console.log('hello world how are you all')
-document.write('hello this is new js file for js practice ')
+function dataLoad(){
+    fetch('http://127.0.0.1:5500/json.txt')
+    .then(res => res.text())
+    .then(data =>getData(data))
+}
+function getData(data){
+document.getElementById('click-me').addEventListener('click', function(){
+const showData = document.getElementById('show-data')
+const p = document.createElement('p');
+p.classList.add('style')
+p.innerHTML= data;
+showData.append(p)
+
+})}
+dataLoad()
