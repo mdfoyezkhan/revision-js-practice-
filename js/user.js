88d5fc4b9e71a467386e8e96f60bb060dc1481f2
foyez khan <mdfoyezkn@gmail.com>
2026-05-04 21:52:11 +0600

    practice for of loop

--- js/user.js
+++ js/user.js
@@ -1,7 +1,10 @@
 function loadAPIData(){
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(res =>res.json())
-    .then(data =>displayData(data))
+    .then(data =>{
+        displayData(data)
+        forLoop(data)
+    })
 }
 function displayData(data){
     const nameDiv =document.getElementById('name-div')
@@ -10,5 +13,16 @@ function displayData(data){
         p.innerText=user.name;
         nameDiv.append(p)
     })
+}
+
+function forLoop(data){
+    const nameDiv=document.getElementById('name-div')
+    const users =data;
+for(const user of users){
+    const p =document.createElement('p');
+    p.innerText=user.phone;
+    nameDiv.append(p)
+
+}
 }
 
